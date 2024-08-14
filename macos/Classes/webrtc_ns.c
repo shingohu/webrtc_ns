75ecd6015eb8da3f312c30e31ebed3895051e8ab
shingohu <shingohu@163.com>
2024-08-14 13:42:58 +0800

    新增macos平台

---
+++ macos/Classes/webrtc_ns.c
@@ -0,0 +1,4 @@
+// Relative import to be able to reuse the C sources.
+// See the comment in ../webrtc_ns.podspec for more information.
+#include "../../src/webrtc_ns.c"
+#include "../../src/noise_suppression.c"
